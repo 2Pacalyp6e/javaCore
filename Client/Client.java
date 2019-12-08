547b9bf2efc2fa3572d64fba56bbf509c24ba983
2pacalyp6e <pachacuti555@mail.ru>
2019-12-08 15:58:26 +0600

    Дз 6 урок JavaCore

---
+++ Client/Client.java
@@ -0,0 +1,67 @@
+package Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    final String SERVER_ADDRESS = "localhost";
+    final int SERVER_PORT = 8191;
+    Socket socket;
+    Scanner in, console;
+    PrintWriter out;
+
+    public Client() {
+
+        try {
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        if (in.hasNext()) {
+                            String w = in.nextLine();
+                            if (w.equalsIgnoreCase("end session")) break;
+                            System.out.println(w);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Пока!");
+                }
+            }
+        }).start();
+
+        startMessaging();
+    }
+
+    //send messages method
+    public void startMessaging() {
+        String message;
+        console = new Scanner(System.in);
+        System.out.println("Введите сообщение:");
+
+        while (true) {
+            message = console.next();
+            out.println(message);
+            out.flush();
+            if (message.equalsIgnoreCase("Конец")) {
+                try {
+                    console.close();
+                    out.close();
+                    in.close();
+                    socket.close();
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
